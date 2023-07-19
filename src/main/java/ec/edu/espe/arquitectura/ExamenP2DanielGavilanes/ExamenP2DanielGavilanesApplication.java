7a47d965cb0c2be6aa553a4d68612a7eaafe533b
Daniel Alonso Gavilanes Falconi <dagavilanes2@espe.edu.ec>
2023-07-19 13:09:18 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espe/arquitectura/ExamenP2DanielGavilanes/ExamenP2DanielGavilanesApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.ExamenP2DanielGavilanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenP2DanielGavilanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenP2DanielGavilanesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/ExamenP2DanielGavilanes/ExamenP2DanielGavilanesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.ExamenP2DanielGavilanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenP2DanielGavilanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
